93bf8e5a36e110d5cdf6bbf7987b8a4814352fd5
Tushar Jayanti <tusharjayanti@Tushars-MacBook-Pro.local>
2020-10-04 19:54:48 +0530

    Updates

---
+++ precache-manifest.b047ff9cb70247355ddff271541bb33f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7df5f78ed0a68bf704357d5a1e460f31",
+    "url": "/dev-assignment/index.html"
+  },
+  {
+    "revision": "333b386f2981988bc399",
+    "url": "/dev-assignment/static/css/2.f2850afe.chunk.css"
+  },
+  {
+    "revision": "d5c69b5fef26243595e3",
+    "url": "/dev-assignment/static/css/main.15c8dc35.chunk.css"
+  },
+  {
+    "revision": "333b386f2981988bc399",
+    "url": "/dev-assignment/static/js/2.e9b61eed.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/dev-assignment/static/js/2.e9b61eed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5c69b5fef26243595e3",
+    "url": "/dev-assignment/static/js/main.ca96a158.chunk.js"
+  },
+  {
+    "revision": "1f3f9117d4d460348bf7",
+    "url": "/dev-assignment/static/js/runtime-main.5fcfd5e1.js"
+  }
+]);
